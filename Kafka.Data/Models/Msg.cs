298065862d163309c082bf80f480a3ae7570a2fd
Jamie Bowman <mrjamiebowman@gmail.com>
2018-07-29 18:24:57 -0400

    formatting on Msg model.

--- Kafka.Data/Models/Msg.cs
+++ Kafka.Data/Models/Msg.cs
@@ -5,6 +5,7 @@ namespace Kafka.Data.Models
     public class Msg
     {
         public string User { get; set; }
+
         public string Message { get; set; }
 
         public DateTime? TimeStamp { get; set; }
